f2d676a291a9d0de66231684d6cd5e7405bdcb26
Ryneqq <rynekleszczynski@gmail.com>
2017-11-20 13:07:28 +0100

    Fixed bug whitch took wrong signals, still learning error doesnt go down. I think there could be some mistake in calculating the error

--- Assets/Scripts/NN/NeuralNetwork.cs
+++ Assets/Scripts/NN/NeuralNetwork.cs
@@ -6,6 +6,8 @@ public class NeuralNetwork{
     private int[] layers;
     private Matrix[] nn;
 
+    private int beta = 5;
+
     /// <summary>
     /// Constructor takes number of inputs and an array of numbers of neurons in each layer.
     /// Initialize neural network with random wages.
@@ -19,6 +21,7 @@ public class NeuralNetwork{
         for(int i = 1; i < layers.Length; i++){
             nn[i] = Matrix.RandomMatrix(layers[i-1] + 1, layers[i], 0.1f);                 
         } 
+
         // Display your nn
         // for(int i = 0; i < nn.Length; i++){
         //     Debug.Log("layer " + i + " rows: " + nn[i].rows + " cols: " + nn[i].cols);     
@@ -34,68 +37,72 @@ public class NeuralNetwork{
     public void Learn(Matrix P, Matrix T, int n){
         int examples = P.cols; // n.o. examples
         int nl = nn.Length; // n.o. layers
-        float lr = .1f; // learning rate
+        float lr = .05f; // learning rate
 
-        for(int l = 0; l < n; l++){
+        for(int m = 0; m < n; m++){
             // ============= Choosing an example ==============
             int next = UnityEngine.Random.Range(0, examples);
             string example = String.Empty;
             for(int i = 0; i < P.rows; i++)
                 example += P[i,next] + "\r\n";
             Matrix signal = Matrix.Parse(example); 
+            // Debug.Log("example: " + example);
             // ================================================
             
             // ==== Signal running through neural network =====
             Matrix[] signals = new Matrix[nl+1];
             signals[0] = Matrix.Parse("-1\r\n" + signal.ToString()); // bias
  
-            for(int i = 0; i < nl; i++){
-                // Debug.Log(" nl " + nl + " i " + i);
+            for(int i = 0; i < nl; i++) {
                 signal = Run(signal, i);
-                signals[i+1] = signal;             
-            }
-            // signal = Run(signal, nl - 1); // response of the network
-            // signals[nl - 1] = signal;
-
-            Array.Reverse(signals); // make it easy to use                 
+                if(i == nl - 1)
+                    signals[i+1] = signal;   
+                else
+                    signals[i+1] = Matrix.Parse("-1\r\n" + signal.ToString());                                      
+            }              
             // ================================================
 
             // ================ Correct anwser ================
             string anwser = String.Empty;
             for(int i = 0; i < T.rows; i++)
                 anwser += T[i,next] + "\r\n";
+            // Debug.Log("anwser: " + anwser);
             // ================================================            
 
             // ===== Error and back propagation algorithm =====
-            Matrix error;         
-            Matrix delta = Matrix.Parse(anwser) - signals[0]; 
+            Matrix delta = Matrix.Parse(anwser) - signals[nl]; 
+            Matrix error = delta.Duplicate();                    
 
+            // for(int k = 0; k < nl+1; k++)
+            //     Debug.Log("signal " + k + "\r\n" + signals[k].ToString());
             for(int k = 0; k < nl; k++){
-                signal = signals[k];
-                error = ActivationFunction(signal);
-                Debug.Log("signal rows: " + signal.rows + " cols " + signal.cols);
-                Debug.Log("error rows: " + error.rows + " cols " + error.cols); 
- 
+                signal = signals[nl - k]; // signal Y2 na start
+
                 for(int i = 0; i < delta.rows; i++){
                     for(int j = 0; j < delta.cols; j++){
-                        error[i,j] *= delta[i,j] * (1 - signal[i,j]);
+                        error[i,j] = delta[i,j] * beta * signal[i,j] * (1 - signal[i,j]); // czy obliczam poprawnie błąd
                     }
                 }
-                
-                Matrix dnn = signal * Matrix.Transpose(error); // to chyba nie ten sygnał            
-                Debug.Log("dnn rows: " + dnn.rows + " cols " + dnn.cols + "\r\n" + dnn.ToString());
-                Debug.Log("nn rows " + nn[nl - 1 - k].rows + " cols " + nn[nl - 1 - k].cols + "\r\n" + nn[nl-1-k].ToString());
-                nn[nl - 1 - k] += dnn;
+
+                Matrix dnn = lr * signals[nl - 1 - k] * Matrix.Transpose(error); // Y1
+                // Debug.Log("dnn rows: " + dnn.rows + " cols " + dnn.cols + "\r\n" + dnn.ToString());    
+                // Debug.Log("nn rows " + nn[nl - 1 - k].rows + " cols " + nn[nl - 1 - k].cols + "\r\n" + nn[nl-1-k].ToString());                            
+                nn[nl - 1 - k] += dnn; 
+                  
+                // Debug.Log("signal rows: " + signal.rows + " cols " + signal.cols);
+                // if(k == 0){}
+                // Debug.Log("error rows: " + error.rows + " cols " + error.cols + "\r\n" + error.ToString());     
 
                 delta = Matrix.RemoveRow(nn[nl - 1 - k], 0) * error; // backpropagation
+                error = delta.Duplicate();
             }
             // ================================================
         }
     }
 
     public Matrix Run(Matrix signal, int index){
-        Debug.Log("-1\r\n" + signal.ToString());
-         Matrix X = Matrix.Parse("-1\r\n" + signal.ToString()); // adding bias        
+        // Debug.Log("-1\r\n" + signal.ToString());
+         Matrix X = Matrix.Parse("-1\r\n" + signal.ToString()); // adding bias      
         // Debug.Log("index: " + index +"\r\n\r\n" +
         //             nn[index].ToString() + "\r\n\r\n" +
         //             "nn rows: " + nn[index].rows + " nn cols: " + nn[index].cols);
@@ -115,7 +122,6 @@ public class NeuralNetwork{
         // sigmoid:
         // 1/(1+exp(-beta*U))
         Matrix Y = U.Duplicate();
-        int beta = 5;
         for(int i = 0; i < Y.rows; i++){
             for(int j = 0; j < Y.cols; j++){
                 Y[i,j] = 1/(1+ System.Math.Exp(-beta * U[i,j]));
--- Assets/Scripts/NN/temp.cs
+++ Assets/Scripts/NN/temp.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 // klasa do testowania działania klasy NeuralNetwork
 public class temp : MonoBehaviour {
 
+	Matrix P = Matrix.Parse("0 0 1 1\r\n0 1 0 1");
+	Matrix T = Matrix.Parse("0 1 1 0");
+	Matrix Pr = Matrix.Parse("1\r\n0");
+	NeuralNetwork nn;
+	
+
+
 	// Use this for initialization
 	void Start () {
-		Matrix P = Matrix.Parse("0 0 1 1\r\n0 1 0 1");
-		Matrix T = Matrix.Parse("0 1 1 0\r\n0 1 0 1");
-		Matrix Pr = Matrix.Parse("0\r\n1");
 
 		int[] layers = new int[2];
-		// layers[0] = 20;
 		layers[0] = 2;
-		layers[1] = 2;		
-        var nn = new NeuralNetwork(2, layers);
+		layers[1] = 1;
+		// layers[2] = 1;		
+		nn = new NeuralNetwork(2, layers);		
+				
 
-		Debug.Log(nn.Run(Pr).ToString());
-		nn.Learn(P,T,1);
-		Debug.Log(nn.Run(Pr).ToString());
+		Debug.Log("Before learning");
+		Debug.Log("Correct: 0, Network response: " + nn.Run(Matrix.Parse("0\r\n0")).ToString());
+		Debug.Log("Correct: 1, Network response: " + nn.Run(Matrix.Parse("1\r\n0")).ToString());
+		Debug.Log("Correct: 1, Network response: " + nn.Run(Matrix.Parse("0\r\n1")).ToString());		
+		Debug.Log("Correct: 0, Network response: " + nn.Run(Matrix.Parse("1\r\n1")).ToString());		
+		
+		// int wartosc = 0;
+		// Matrix tab = new Matrix(9,9);
+		// for(int k = 0; k < 3; k++){
+		// 	for(int i = k*3, l = 0; l < 3; i++, l++){
+		// 		for(int j = k*3; j < k*3+3; j++){
+		// 			tab[i,j] = wartosc++;
+		// 			Debug.Log("i: " + i + " j: " + j);
+		// 		}
+		// 	}
+		// }
+		// Debug.Log(tab.ToString());
+	}
 
+	public void Learn(){
+		nn.Learn(P,T,10000);
+		Debug.Log("After learning");
+		Debug.Log("Correct: 0, Network response: " + nn.Run(Matrix.Parse("0\r\n0")).ToString());
+		Debug.Log("Correct: 1, Network response: " + nn.Run(Matrix.Parse("1\r\n0")).ToString());
+		Debug.Log("Correct: 1, Network response: " + nn.Run(Matrix.Parse("0\r\n1")).ToString());		
+		Debug.Log("Correct: 0, Network response: " + nn.Run(Matrix.Parse("1\r\n1")).ToString());
 	}
 }
